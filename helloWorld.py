c9a0423ed61d2694cfdcc24a1e2bbbefe61b9687
Jeferson Andrade <jefersonandradde@users.noreply.github.com>
2015-03-19 22:39:07 -0300

    Update helloWorld.py
    Indentação com tabs invés de espaços (Evita exceções em tempo de execução).

--- helloWorld.py
+++ helloWorld.py
@@ -1,4 +1,4 @@
 turma = ["Andrade", "André", "Daiane", "Dailma", "Diego", "Emmanuel", "Joelson", "Márcio", "Milena", "Rogério", "Ruivo", "Tobias"]
 
 for estudante in turma:
-  print(estudante)
+	print(estudante)
